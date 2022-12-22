0e3fede6a97ab39b49434dbca99bc1bda618c356
lizuncong <2900170896@qq.com>
2022-12-22 23:22:26 +0800

    feat: stash

--- src/main.js
+++ src/main.js
@@ -6,15 +6,34 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, './airclass/preload.js'),
-      contextIsolation: true
+      // preload: path.join(__dirname, './airclass/preload.js'),
+      // contextIsolation: true
+      nativeWindowOpen: true
+
     },
   });
 
   // AirClass
-  new AirclassManagement();
+  // new AirclassManagement();
+  win.webContents.on('new-window', (event, url, frameName, disposition, options, additionalFeatures) => {
+    console.log('main...', frameName)
+    if (frameName === 'modal') {
 
-  win.loadFile(path.join(__dirname, './index.html'));
+      // open window as modal
+      event.preventDefault()
+      Object.assign(options, {
+        modal: true,
+        // parent: win,
+        // width: 100,
+        // height: 100
+      })
+    }
+    event.newGuest = new BrowserWindow(options)
+  })
+
+
+  win.loadURL('http://192.168.1.103:3000/')
+  // win.loadFile(path.join(__dirname, './index.html'));
   win.webContents.openDevTools()
 };
 
