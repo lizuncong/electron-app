e4ac068494598369244acc3bdd99b9b98cdb25b2
lizuncong <2900170896@qq.com>
2023-02-19 22:03:44 +0800

    feat: 内存开销

--- docs/electron性能.md
+++
@@ -1,7 +0,0 @@
-## loadURL 方式会多出一个渲染进程
-
-## 打开关闭窗口，内存没有释放
-
-## window open 是能对性能最好的一种方式
-
-不论 window open 打开多少个窗口，自始至终只有一个 render process。我们都知道，创建一个进程都是需要内存开销的。通过 new BrowserWindow 创建窗口会为每个窗口创建一个 render process 进程
---
+++ src/memory-usage/preload.js
@@ -0,0 +1,36 @@
+const { contextBridge, ipcRenderer } = require("electron");
+const { webFrame } = require("electron");
+
+const getProcessMemoryUsage = require("./getProcessMemoryUsage");
+
+const getWebFrameResourceUsage = require("./getWebFrameResourceUsage");
+
+window.addEventListener("DOMContentLoaded", () => {
+  getProcessMemoryUsage("processmemoryusage");
+  getWebFrameResourceUsage("webframeresourceusage");
+
+  setTimeout(() => {
+    const processpid = document.getElementById("processpid");
+    if (processpid) processpid.innerHTML = `渲染进程id：${process.pid}`;
+
+    ipcRenderer.invoke("get-main-process-id").then((res) => {
+      const mainprocessid = document.getElementById("mainprocessid");
+      if (mainprocessid)
+        mainprocessid.innerHTML = `electron主进程id：${res.pid}`;
+    });
+  }, 1000);
+  setTimeout(() => {
+    const btn = document.getElementById("clearwebframe");
+    btn &&
+      btn.addEventListener("click", (e) => {
+        webFrame.clearCache();
+        console.log("CLEARED");
+      });
+
+    const openBtn = document.getElementById("newbrowserwindow");
+    openBtn &&
+      openBtn.addEventListener("click", (e) => {
+        ipcRenderer.send("open-window");
+      });
+  }, 2000);
+});
