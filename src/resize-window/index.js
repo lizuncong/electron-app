ed28faf284fa7a585912899cfd4aa7ac7a5df0a1
p_zuncongli <p_zuncongli@tencent.com>
2024-05-24 18:53:50 +0800

    fix: stash

--- src/resize-window/index.js
+++ src/resize-window/index.js
@@ -1,12 +1,12 @@
 // require('./memory-usage')
-const { app, ipcMain, BrowserWindow } = require("electron");
+const { app, ipcMain, BrowserView, BrowserWindow } = require("electron");
 const path = require("path");
 const { parse } = require("qs");
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 85,
-    height: 85,
+    width: 100,
+    height: 100,
     frame: false,
     resizable: false,
     movable:true,
@@ -14,31 +14,39 @@ function createWindow () {
       contextIsolation: true,
       preload: path.join(__dirname, './preload.js')
     },
-    transparent: true
+    // transparent: true
   })
-  win.setContentBounds({
-    x: -800,
-    y: -800,
-    width: 1600,
-    height: 1600,
+  // win.setContentBounds({
+  //   x: 100,
+  //   y: 100,
+  //   width: 500,
+  //   height: 500,
+  // });
+  const view = new BrowserView()
+  win.setBrowserView(view)
+  view.setBounds({ x: -350, y: -350, width: 800, height: 800 })
+  view.webContents.loadURL('http://localhost:3000/')
+  view.webContents.openDevTools({
+    mode: 'undocked'
   });
-  ipcMain.on('set-bounds', (event, params) => {
+    // console.log('init...', win.getBrowserView())
+  ipcMain.on('se t-bounds', (event, params) => {
     const webContents = event.sender
     const win = BrowserWindow.fromWebContents(webContents)
     console.log('setboun ds....', params, params.width)
     const {width, height, originX, originY} = params;
     const currentBounds = win.getBounds();
-    win.setContentSize(width, height)
+    // win.setContentSize(width, height)
 
     const {x: currentX, y: currentY, width: currentW, height: currentH} = currentBounds
-    win.setBounds({ x: currentX - originX + currentW, y: currentY - originY + currentH, width, height })
+    // win.setBounds({ x: currentX - originX + currentW, y: currentY - originY + currentH, width, height })
   })
   // win.setIgnoreMouseEvents(true)
 
-  win.webContents.openDevTools({
-    mode: 'undocked'
-  });
-  win.loadURL('http://localhost:3000/')
+  // win.webContents.openDevTools({
+  //   mode: 'undocked'
+  // });
+  // win.loadURL('http://localhost:3000/')
 }
 
 app.whenReady().then(() => {
